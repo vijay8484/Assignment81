43792b5cd118c26492922efab051ffc2f9f0d370
Your NameVijay <vijay8484patil@gmail.com>
2019-01-04 23:31:02 +0530

    Assigment 81

---
+++ src/app/child/child.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-child',
+  templateUrl: './child.component.html',
+  styleUrls: ['./child.component.css']
+})
+export class ChildComponent implements OnInit {
+
+  // Create object of event class
+  @Output() public MyEvent = new EventEmitter();
+ 
+  // Action listener for button
+  public SendEvent()
+  {
+    // Send the event to parent
+    this.MyEvent.emit('Message from child');
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
